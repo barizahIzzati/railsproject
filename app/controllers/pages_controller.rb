f6bb6b3f6907da94033b76027fd90564a4282285
barizahIzzati <bariza.izzati@gmail.com>
2019-11-18 16:39:27 +0800

    current user page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def info
+    if params[:category].blank?
+      @products = Product.all.order("created_at DESC")
+      @products = Product.search(params[:search])
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @products = Product.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 end
