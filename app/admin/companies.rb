91ffbf10b063d3ba1818fccc7e3c237bc55f4a64
barizahIzzati <bariza.izzati@gmail.com>
2019-12-18 11:28:05 +0800

    update

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register Company do
-  permit_params :company_name, :register_no, :address, :city, :state, :postcode, :phone_no, :fax, :email, :website
+  # permit_params :company_name, :register_no, :address, :city, :state, :postcode, :phone_no, :fax, :email, :website
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :company_name, :register_no, :address, :city, :state, :postcode, :phone_no, :fax, :email, :website, :user_id
+  permit_params :company_name, :register_no, :address, :city, :state, :postcode, :phone_no, :fax, :email, :website, :user_id
   #
   # or
   #
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register User do
-  permit_params :email, :reset_password_token
+  # permit_params :email, :reset_password_token
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   #
   # or
   #
